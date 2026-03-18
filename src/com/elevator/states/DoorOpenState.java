58f99c1a24715b5506df643f748507026a97e11d
Anshu Nigam <nigamanshu188@gmail.com>
2026-03-18 14:35:45 +0530

    Added DoorOpenState to handle elevator door operations

---
+++ src/com/elevator/states/DoorOpenState.java
@@ -0,0 +1,15 @@
+package com.elevator.states;
+
+import com.elevator.model.Elevator;
+
+public class DoorOpenState implements ElevatorState {
+
+    @Override
+    public void handleRequest(Elevator elevator, int destinationFloor) {
+
+        System.out.println("Door opening at floor " + elevator.getCurrentFloor());
+
+        // After door operation, go back to idle
+        elevator.setState(new IdleState());
+    }
+}
